a16fb56ab6a8683829b2e92fc8f84b7bf8121b99
idktrolled <camilocarlos1100@gmail.com>
2026-03-20 15:18:25 -0500

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -541,8 +541,6 @@ class PlayState extends MusicBeatState
 		FlxG.cameras.reset(camGame);
 		FlxG.cameras.add(camHUD);
 
-		FlxCamera.defaultCameras = [camGame];
-
 		persistentUpdate = true;
 		persistentDraw = true;
 
